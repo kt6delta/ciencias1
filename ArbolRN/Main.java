15fc8e5a255f19bbf7bca5e66dc1d950b67a0e3e
kt6delta <lcpreciadob@correo.udistrital.edu.co>
2023-02-25 01:33:11 -0500

    funcional

--- ArbolRN/Main.java
+++ ArbolRN/Main.java
@@ -1,4 +1,3 @@
-
 package ArbolRN;
 
 import java.util.Random;
@@ -6,20 +5,21 @@ import java.util.Random;
 public class Main {
 
   public static void main(String[] args) {
-    Random ran = new Random();
+    Random random = new Random();
     Arbol Arbolito = new Arbol();
-    /*21, 28, 15, 8, 2, 18, 19, 6, 0, 26, 21, 17, 30, 7, 13, 25, 16, 5, 32*/
-    for (int i = 0; i < 10; i++) {
-        Arbolito.insertar(ran.nextInt(56));
+    int datos[]={21, 28, 15, 8, 2, 18, 19, 6, 0, 26, 21, 17, 30, 7, 13, 25, 16, 5, 32};
+    for (int i = 0; i < datos.length; i++) {
+      Arbolito.insertar(datos[i]);
+      //Arbolito.insertar(random.nextInt(56));
     }
-    Arbolito.eliminar(0);
-    Arbolito.recorrer(Recorrido.INORDER);
-    System.out.println("preorder");
-    Arbolito.recorrer(Recorrido.PREORDER);
+    //Arbolito.eliminar(0);
+    Arbolito.recorrer(Recorrido.POSTORDER);//Left, Right, Nodo
+    System.out.println();
+    Arbolito.recorrer(Recorrido.INORDER); //Left, Nodo, Right
+    System.out.println();
+    Arbolito.recorrer(Recorrido.PREORDER);//Nodo, Left,Right
     System.out.println();
     Arbolito.recorrer(Recorrido.HOJAS);
-    System.out.println();
-    Arbolito.recorrer(Recorrido.POSTORDER);
     System.out.println();
     System.out.println(Arbolito.buscar(5));
   }
